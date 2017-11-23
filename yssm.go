cbd32ffce208573d21bb2f59b882d82cf106e053
Fan Yang <missdeer@dfordsoft.com>
2017-11-23 10:31:24 +0800

    (+)support yssm

---
+++ yssm.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"regexp"
+	"time"
+
+	"github.com/dfordsoft/golib/ebook"
+	"github.com/dfordsoft/golib/httputil"
+)
+
+func init() {
+	registerNovelSiteHandler(&novelSiteHandler{
+		Title: `幼狮书盟`,
+		MatchPatterns: []string{
+			`http://www\.yssm\.org/uctxt/[0-9]+/[0-9]+/`,
+		},
+		Download: func(u string) {
+			dlPage := func(u string) (c []byte) {
+				var err error
+				headers := map[string]string{
+					"Referer":                   "http://www.yssm.org/",
+					"User-Agent":                "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0",
+					"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+					"Accept-Language":           `en-US,en;q=0.8`,
+					"Upgrade-Insecure-Requests": "1",
+				}
+				c, err = httputil.GetBytes(u, headers, 60*time.Second, 3)
+				if err != nil {
+					return
+				}
+
+				c = bytes.Replace(c, []byte("\r\n"), []byte(""), -1)
+				c = bytes.Replace(c, []byte("\r"), []byte(""), -1)
+				c = bytes.Replace(c, []byte("\n"), []byte(""), -1)
+				leadingStr := `<div id="content">`
+				idx := bytes.Index(c, []byte(leadingStr))
+				if idx > 1 {
+					c = c[idx+len(leadingStr):]
+				}
+				idx = bytes.Index(c, []byte("</div>"))
+				if idx > 1 {
+					c = c[:idx]
+				}
+				c = bytes.Replace(c, []byte("<br/>　　"), []byte("</p><p>"), -1)
+				return
+			}
+			tocURL := u
+			headers := map[string]string{
+				"Referer":                   "http://www.yssm.org/",
+				"User-Agent":                "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0",
+				"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+				"Accept-Language":           `en-US,en;q=0.8`,
+				"Upgrade-Insecure-Requests": "1",
+			}
+			b, err := httputil.GetBytes(tocURL, headers, 60*time.Second, 3)
+			if err != nil {
+				return
+			}
+
+			mobi := &ebook.Mobi{}
+			mobi.Begin()
+
+			var title string
+			r, _ := regexp.Compile(`<dd>\s<a\sstyle=""=style=""\shref="([0-9]+\.html)">([^<]+)</a></dd>$`)
+			re, _ := regexp.Compile(`<h1>([^<]+)</h1>$`)
+			scanner := bufio.NewScanner(bytes.NewReader(b))
+			scanner.Split(bufio.ScanLines)
+			for scanner.Scan() {
+				l := scanner.Text()
+				if title == "" {
+					ss := re.FindAllStringSubmatch(l, -1)
+					if len(ss) > 0 && len(ss[0]) > 0 {
+						s := ss[0]
+						title = s[1]
+						mobi.SetTitle(title)
+						continue
+					}
+				}
+				if r.MatchString(l) {
+					ss := r.FindAllStringSubmatch(l, -1)
+					s := ss[0]
+					finalURL := fmt.Sprintf("%s%s", tocURL, s[1])
+					c := dlPage(finalURL)
+					mobi.AppendContent(s[2], finalURL, string(c))
+					fmt.Println(s[2], finalURL, len(c), "bytes")
+				}
+			}
+			mobi.End()
+		},
+	})
+}
