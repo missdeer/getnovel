4dd571f6e5aafddc98f871ae0fb30deda22a009c
Fan Yang <missdeer@dfordsoft.com>
2017-12-27 10:28:27 +0800

    (*)support variant page size

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/dfordsoft/golib/ebook"
+	flags "github.com/jessevdk/go-flags"
 )
 
 type tocPattern struct {
@@ -60,6 +61,17 @@ func listCommandHandler() {
 	}
 }
 
+// Options for all command line options
+type Options struct {
+	Format          string  `short:"f" long:"format" description:"set generated file format, candidate values: mobi, epub, pdf"`
+	List            bool    `short:"l" long:"list" description:"list supported novel websites"`
+	LeftMargin      float64 `long:"leftMargin" description:"set left margin for PDF format"`
+	TopMargin       float64 `long:"topMargin" description:"set top margin for PDF format"`
+	PageType        string  `long:"pageType" description:"set page type for PDF format, candidate values: a0, a1, a2, a3, a4, a5, a6, b0, b1, b2, b3, b4, b5, b6, c0, c1, c2, c3, c4, c5, c6, dxg(=a4), 6inch(90mm x 117mm), 7inch, 10inch(=a4)"`
+	TitleFontSize   int     `long:"titleFontSize" description:"set title font point size for PDF format"`
+	ContentFontSize int     `long:"contentFontSize" description:"set content font point size for PDF format"`
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("使用方法：\n\tgetnovel 小说目录网址")
@@ -67,23 +79,49 @@ func main() {
 		return
 	}
 
-	var novelURL string
-	for i := 1; i < len(os.Args); i++ {
-		switch os.Args[i] {
-		case "list":
-			listCommandHandler()
-			return
-		case "dxg":
-			gen = &ebook.Pdf{}
-		default:
-			_, e := url.Parse(os.Args[i])
-			if e == nil {
-				novelURL = os.Args[i]
-			}
-		}
+	opts := Options{
+		Format:          "mobi",
+		List:            false,
+		LeftMargin:      10,
+		TopMargin:       10,
+		PageType:        "a4",
+		TitleFontSize:   24,
+		ContentFontSize: 18,
 	}
 
-	if gen == nil {
+	args, err := flags.Parse(&opts)
+	if err != nil {
+		return
+	}
+
+	if opts.List {
+		listCommandHandler()
+		return
+	}
+
+	if len(args) == 0 {
+		fmt.Println("使用方法：\n\tgetnovel 小说目录网址")
+		listCommandHandler()
+		return
+	}
+
+	novelURL := args[0]
+	_, e := url.Parse(novelURL)
+	if e != nil {
+		fmt.Println("使用方法：\n\tgetnovel 小说目录网址")
+		listCommandHandler()
+		return
+	}
+
+	switch opts.Format {
+	case "mobi":
+		gen = &ebook.Mobi{}
+	case "pdf":
+		gen = &ebook.Pdf{}
+		gen.SetFontSize(opts.TitleFontSize, opts.ContentFontSize)
+		gen.SetMargins(opts.LeftMargin, opts.TopMargin)
+		gen.SetPageType(opts.PageType)
+	default:
 		gen = &ebook.Mobi{}
 	}
 
