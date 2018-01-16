91372b64d3c38a22cddb69f6a70986e9ee1c740a
Fan Yang <missdeer@dfordsoft.com>
2018-01-16 11:51:43 +0800

    (-)remove buzz

--- piaotian.go
+++ piaotian.go
@@ -38,6 +38,7 @@ func init() {
 				c = bytes.Replace(c, []byte("\r"), []byte(""), -1)
 				c = bytes.Replace(c, []byte("\n"), []byte(""), -1)
 				c = bytes.Replace(c, []byte(`更多更快章节请到。`), []byte(""), -1)
+				c = bytes.Replace(c, []byte(`第一时间更新`), []byte(""), -1)
 				idx := bytes.Index(c, []byte("</tr></table><br>&nbsp;&nbsp;&nbsp;&nbsp;"))
 				if idx > 1 {
 					c = c[idx+17:]
