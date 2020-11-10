2bd3c77bde4360536d15f32015832c5fe346d14f
Fan Yang <fyang3@cisco.com>
2020-11-10 13:05:31 +0800

    (*)add option to skip insecure verification

--- main.go
+++ main.go
@@ -13,33 +13,35 @@ import (
 
 	"github.com/jessevdk/go-flags"
 	"github.com/missdeer/getnovel/ebook"
+	"github.com/missdeer/golib/httputil"
 )
 
 // Options for all command line options, long name must match field name
 type Options struct {
-	ListenAndServe  string  `long:"listenAndServe" description:"set http listen and serve address, example: :8080"`
-	Format          string  `short:"f" long:"format" description:"set generated file format, candidate values: mobi, epub, pdf"`
-	List            bool    `short:"l" long:"list" description:"list supported novel websites"`
-	LeftMargin      float64 `long:"leftMargin" description:"set left margin for PDF format"`
-	TopMargin       float64 `long:"topMargin" description:"set top margin for PDF format"`
-	PageWidth       float64 `long:"pageWidth" description:"set page width for PDF format(unit: mm)"`
-	PageHeight      float64 `long:"pageHeight" description:"set page height for PDF format(unit: mm)"`
-	PageType        string  `short:"p" long:"pageType" description:"set page type for PDF format, add suffix to output file name"`
-	TitleFontSize   int     `long:"titleFontSize" description:"set title font point size for PDF format"`
-	ContentFontSize int     `long:"contentFontSize" description:"set content font point size for PDF format"`
-	LineSpacing     float64 `long:"lineSpacing" description:"set line spacing rate for PDF format"`
-	PagesPerFile    int     `long:"pagesPerFile" description:"split the big single PDF file to several smaller PDF files, how many pages should be included in a file, 0 means don't split"`
-	ChaptersPerFile int     `long:"chaptersPerFile" description:"split the big single PDF file to several smaller PDF files, how many chapters should be included in a file, 0 means don't split"`
-	FontFile        string  `long:"fontFile" description:"set TTF font file path"`
-	RetryCount      int     `short:"r" long:"retryCount" description:"download retry count"`
-	Timeout         int     `short:"t" long:"timeout" description:"download timeout seconds"`
-	ParallelCount   int64   `long:"parallelCount" description:"parallel count for downloading"`
-	ConfigFile      string  `short:"c" long:"configFile" description:"read configurations from local file"`
-	OutputFile      string  `short:"o" long:"outputFile" description:"output file path"`
-	FromChapter     int     `long:"fromChapter" description:"from chapter"`
-	FromTitle       string  `long:"fromTitle" description:"from title"`
-	ToChapter       int     `long:"toChapter" description:"to chapter"`
-	ToTitle         string  `long:"toTitle" description:"to title"`
+	InsecureSkipVerify bool    `long:"insecureSkipVerify" description:"if true, TLS accepts any certificate"`
+	ListenAndServe     string  `long:"listenAndServe" description:"set http listen and serve address, example: :8080"`
+	Format             string  `short:"f" long:"format" description:"set generated file format, candidate values: mobi, epub, pdf"`
+	List               bool    `short:"l" long:"list" description:"list supported novel websites"`
+	LeftMargin         float64 `long:"leftMargin" description:"set left margin for PDF format"`
+	TopMargin          float64 `long:"topMargin" description:"set top margin for PDF format"`
+	PageWidth          float64 `long:"pageWidth" description:"set page width for PDF format(unit: mm)"`
+	PageHeight         float64 `long:"pageHeight" description:"set page height for PDF format(unit: mm)"`
+	PageType           string  `short:"p" long:"pageType" description:"set page type for PDF format, add suffix to output file name"`
+	TitleFontSize      int     `long:"titleFontSize" description:"set title font point size for PDF format"`
+	ContentFontSize    int     `long:"contentFontSize" description:"set content font point size for PDF format"`
+	LineSpacing        float64 `long:"lineSpacing" description:"set line spacing rate for PDF format"`
+	PagesPerFile       int     `long:"pagesPerFile" description:"split the big single PDF file to several smaller PDF files, how many pages should be included in a file, 0 means don't split"`
+	ChaptersPerFile    int     `long:"chaptersPerFile" description:"split the big single PDF file to several smaller PDF files, how many chapters should be included in a file, 0 means don't split"`
+	FontFile           string  `long:"fontFile" description:"set TTF font file path"`
+	RetryCount         int     `short:"r" long:"retryCount" description:"download retry count"`
+	Timeout            int     `short:"t" long:"timeout" description:"download timeout seconds"`
+	ParallelCount      int64   `long:"parallelCount" description:"parallel count for downloading"`
+	ConfigFile         string  `short:"c" long:"configFile" description:"read configurations from local file"`
+	OutputFile         string  `short:"o" long:"outputFile" description:"output file path"`
+	FromChapter        int     `long:"fromChapter" description:"from chapter"`
+	FromTitle          string  `long:"fromTitle" description:"from title"`
+	ToChapter          int     `long:"toChapter" description:"to chapter"`
+	ToTitle            string  `long:"toTitle" description:"to title"`
 }
 
 type tocPattern struct {
@@ -131,21 +133,22 @@ func main() {
 	}
 
 	opts = Options{
-		Format:          "mobi",
-		List:            false,
-		LeftMargin:      10,
-		TopMargin:       10,
-		PageHeight:      841.89,
-		PageWidth:       595.28,
-		TitleFontSize:   24,
-		ContentFontSize: 18,
-		LineSpacing:     1.2,
-		PagesPerFile:    0,
-		ChaptersPerFile: 0,
-		FontFile:        filepath.Join("fonts", "CustomFont.ttf"),
-		RetryCount:      3,
-		Timeout:         60,
-		ParallelCount:   10,
+		InsecureSkipVerify: false,
+		Format:             "mobi",
+		List:               false,
+		LeftMargin:         10,
+		TopMargin:          10,
+		PageHeight:         841.89,
+		PageWidth:          595.28,
+		TitleFontSize:      24,
+		ContentFontSize:    18,
+		LineSpacing:        1.2,
+		PagesPerFile:       0,
+		ChaptersPerFile:    0,
+		FontFile:           filepath.Join("fonts", "CustomFont.ttf"),
+		RetryCount:         3,
+		Timeout:            60,
+		ParallelCount:      10,
 	}
 
 	args, err := flags.Parse(&opts)
@@ -202,6 +205,8 @@ func main() {
 			return
 		}
 	}
+
+	httputil.SetInsecureSkipVerify(opts.InsecureSkipVerify)
 
 	downloadedChannel := make(chan bool)
 	downloadCount := 0
