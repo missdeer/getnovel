69399f095456f8ce732e1174307fa793a5d0cd16
Fan Yang <missdeer@gmail.com>
2022-12-24 12:31:50 +0800

    add missing file

---
+++ novel.go
@@ -0,0 +1,34 @@
+package main
+
+import "github.com/missdeer/getnovel/ebook"
+
+type TOCPattern struct {
+	Host            string
+	BookTitle       string
+	BookTitlePos    int
+	Item            string
+	ArticleTitlePos int
+	ArticleURLPos   int
+	IsAbsoluteURL   bool
+}
+
+type PageContentMarker struct {
+	Host  string
+	Start []byte
+	End   []byte
+}
+
+type NovelChapterInfo struct {
+	Index int
+	Title string
+	URL   string
+}
+
+type NovelSiteHandler struct {
+	Title                    string
+	MatchPatterns            []string
+	PreprocessChapterListURL func(string) string                                // (original url) -> final url
+	ExtractChapterList       func(string, []byte) (string, []*NovelChapterInfo) // (url, raw page content) (title, chapters)
+	ExtractChapterContent    func([]byte) []byte                                // (raw page content) -> cleanup content
+	Download                 func(string, ebook.IBook)
+}
