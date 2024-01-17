c9e45871ed426b39d72663b8de2d5d540251a231
Fan Yang <missdeer@gmail.com>
2024-01-17 15:57:32 +0800

    fix build lua54 on macOS

--- golua/lua/lua54/lua.h
+++ golua/lua/lua54/lua.h
@@ -154,6 +154,8 @@ LUA_API lua_State *(lua_newstate) (lua_Alloc f, void *ud);
 LUA_API void       (lua_close) (lua_State *L);
 LUA_API lua_State *(lua_newthread) (lua_State *L);
 LUA_API int        (lua_resetthread) (lua_State *L);
+LUA_API int        (lua_closethread)(lua_State *L, lua_State *from);
+LUA_API void       (lua_closeslot)(lua_State *L, int idx);
 
 LUA_API lua_CFunction (lua_atpanic) (lua_State *L, lua_CFunction panicf);
 
