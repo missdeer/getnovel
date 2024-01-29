060ec5f55e29d878b2947dbeeec20129a3a17607
Fan Yang <missdeer@gmail.com>
2024-01-29 10:52:16 +0800

    support command line option to specify lua version to build with

--- build.bat
+++ build.bat
@@ -8,14 +8,47 @@ set "PATH=%PATH%;H:\msys64\mingw64\bin"
 set CGO_ENABLED=1
 for /F "tokens=*" %%R in ('git rev-parse --short HEAD') do set REV=%%R
 for /F "tokens=*" %%A in ('date /T') do set TODAY=%%A
+
+if "%1"=="" goto buildall
+if "%1"=="51" goto build51
+if "%1"=="52" goto build52
+if "%1"=="53" goto build53
+if "%1"=="54" goto build54
+if "%1"=="jit" goto buildjit
+@goto end
+
+:buildall
+@call :build 51
+@call :build 52
+@call :build 53
+@call :build 54
+@call :build jit
+@goto end
+
+:build51
+@call :build 51
+@goto end
+
+:build52
+@call :build 52
+@goto end
+
+:build53
+@call :build 53
+@goto end
+
+:build54
+@call :build 54
+@goto end
+
+:buildjit
+@call :build jit
+@goto end
+
+:build
+@go clean -tags lua%1
 @echo on
-@go clean -tags lua51
-go build -ldflags="-s -w -X main.sha1ver=%REV% -X 'main.buildTime=%TODAY%'" -tags lua51 -o getnovel-lua51.exe
-@go clean -tags lua52
-go build -ldflags="-s -w -X main.sha1ver=%REV% -X 'main.buildTime=%TODAY%'" -tags lua52 -o getnovel-lua52.exe
-@go clean -tags lua53
-go build -ldflags="-s -w -X main.sha1ver=%REV% -X 'main.buildTime=%TODAY%'" -tags lua53 -o getnovel-lua53.exe
-@go clean -tags lua54
-go build -ldflags="-s -w -X main.sha1ver=%REV% -X 'main.buildTime=%TODAY%'" -tags lua54 -o getnovel-lua54.exe
-@go clean -tags luajit
-go build -ldflags="-s -w -X main.sha1ver=%REV% -X 'main.buildTime=%TODAY%'" -tags luajit -o getnovel-luajit.exe
+go build -ldflags="-s -w -X main.sha1ver=%REV% -X 'main.buildTime=%TODAY%'" -tags lua%1 -o getnovel-lua%1.exe
+@goto :eof
+
+:end
--- build.sh
+++ build.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
+
 OS=$(uname -s)
-if [ "$OS" == "Darwin" ]; then
-	for lua in lua51 lua52 lua53 lua54 luajit; do
-		env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go clean --tags ${lua}
-		env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w -X main.sha1ver=$(git rev-parse --short HEAD) -X 'main.buildTime=$(date)'" --tags ${lua} -o getnovel-${lua}-arm64
-		env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go clean --tags ${lua}
-		env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w -X main.sha1ver=$(git rev-parse --short HEAD) -X 'main.buildTime=$(date)'" --tags ${lua} -o getnovel-${lua}-amd64
-		lipo -create -output getnovel-${lua} getnovel-${lua}-amd64 getnovel-${lua}-arm64
-		rm getnovel-${lua}-arm64 getnovel-${lua}-amd64
-	done
+
+# Function to compile for a given Lua tag
+compile_lua() {
+	local lua_tag=$1
+	if [ "$OS" == "Darwin" ]; then
+		ARCH=$(uname -m)
+		if [ "$ARCH" == "arm64" ]; then
+			# Compile for ARM64 and x86_64 and create a universal binary
+			env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go clean --tags ${lua_tag}
+			env CGO_ENABLED=1 GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w -X main.sha1ver=$(git rev-parse --short HEAD) -X 'main.buildTime=$(date)'" --tags ${lua_tag} -o getnovel-${lua_tag}-arm64
+			env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go clean --tags ${lua_tag}
+			env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w -X main.sha1ver=$(git rev-parse --short HEAD) -X 'main.buildTime=$(date)'" --tags ${lua_tag} -o getnovel-${lua_tag}-amd64
+			lipo -create -output getnovel-${lua_tag} getnovel-${lua_tag}-amd64 getnovel-${lua_tag}-arm64
+			rm getnovel-${lua_tag}-arm64 getnovel-${lua_tag}-amd64
+		elif [ "$ARCH" == "x86_64" ]; then
+			# Compile only for x86_64
+			env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go clean --tags ${lua_tag}
+			env CGO_ENABLED=1 GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w -X main.sha1ver=$(git rev-parse --short HEAD) -X 'main.buildTime=$(date)'" --tags ${lua_tag} -o getnovel-${lua_tag}
+		fi
+	else
+		# For other OSes
+		env CGO_ENABLED=1 go clean --tags ${lua_tag}
+		env CGO_ENABLED=1 go build -ldflags="-s -w -X main.sha1ver=$(git rev-parse --short HEAD) -X 'main.buildTime=$(date)'" --tags ${lua_tag} -o getnovel-${lua_tag}
+	fi
+}
+
+# Check if a tag is provided
+if [ -n "$1" ]; then
+	compile_lua "lua$1"
 else
 	for lua in lua51 lua52 lua53 lua54 luajit; do
-		env CGO_ENABLED=1 go clean --tags ${lua}
-		env CGO_ENABLED=1 go build -ldflags="-s -w -X main.sha1ver=$(git rev-parse --short HEAD) -X 'main.buildTime=$(date)'" --tags ${lua} -o getnovel-${lua}
+		compile_lua ${lua}
 	done
 fi
